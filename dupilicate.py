aac0c511ff2036696aff8a984c822a51d18656b5
senthilcse1807 <senthilcse111@gmail.com>
2026-03-18 14:50:17 +0530

    Add files via upload

---
+++ dupilicate.py
@@ -0,0 +1,35 @@
+n = int(input("enter the number of input:"))
+arr = []
+
+for i in range(1,n+1,1):
+    t = input("n:")
+    arr.append(t)
+    
+def insertion():
+
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j = j-1
+
+        arr[j+1] = key
+
+    print("The sorted array:",arr)
+
+    #dup = []
+    uni = []
+
+    for i in arr:
+       
+        if not uni or i not in uni:
+            uni.append(i)
+        
+    print("the unique array:",uni)
+ 
+        
+
+insertion()
+            
